cb28f91a62ab8d5626679d8d998ed03408e0a006
Macha Celeste <shetah1@yahoo.com>
2025-05-01 12:01:05 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ExpPlugin;
+
+[BepInPlugin("com.machaceleste.expplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<int> playerExp;
+    public static ConfigEntry<int> guildExp;
+
+    private void Awake()
+    {
+        playerExp = Config.Bind("Main", "Player EXP", 200, new ConfigDescription("", new AcceptableValueRange<int>(1, 1000)));
+        guildExp = Config.Bind("Main", "Guild EXP", 25, new ConfigDescription("", new AcceptableValueRange<int>(1, 1000)));
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.expplugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/MailHelperServerPatch.cs
@@ -0,0 +1,27 @@
+﻿using ExpPlugin;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+[HarmonyPatch]
+public class MailHelperServerPatch
+{
+    [HarmonyPatch(typeof(MailHelperServer), "CheckMailActions")]
+    class CheckMailActionsPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_I4)
+                {
+                    if ((int)instruction.operand == 200)
+                        instruction.operand = Plugin.playerExp.Value;
+                    else if ((int)instruction.operand == 25)
+                        instruction.operand = Plugin.guildExp.Value;
+                }
+                yield return instruction;
+            }
+        }
+    }
+}
